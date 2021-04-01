38ff485c6aaf42756d55ca2e784366412f7d20c5
Sayem Chowdhury <sayem314@gmail.com>
2021-04-01 14:01:33 +0600

    update dependencies

--- server.ts
+++ server.ts
@@ -1,16 +1,15 @@
 import fastify from 'fastify';
+import socketioServer from 'fastify-socket.io';
 import {getFullTextArticle} from './lib/diffbot';
 
 const server = fastify({logger: true});
-
-interface articleQuery {
-  jsonfeed: boolean;
-  url: string;
-}
+server.register(socketioServer);
 
 const articleSchema = {
   schema: {
     querystring: {
+      feedId: 'string',
+      id: 'string',
       url: {
         type: 'string',
         format: 'uri',
@@ -21,17 +20,31 @@ const articleSchema = {
 };
 
 server.get('/v1/article', articleSchema, async (request, reply) => {
-  const {url} = request.query as articleQuery;
-  const articleData = await getFullTextArticle(url);
+  const {url} = request.query as any;
+  const data = await getFullTextArticle(url);
 
-  if (articleData.error) {
-    if (typeof articleData.errorCode === 'number') {
-      reply.statusCode = articleData.errorCode;
+  if (data.error) {
+    if (typeof data.errorCode === 'number') {
+      reply.statusCode = data.errorCode;
     }
-    return new Error(articleData.error);
+    return new Error(data.error);
   }
 
-  return articleData;
+  return data;
+});
+
+server.get('/v1/socket/article', articleSchema, async (request) => {
+  const {feedId, id, url} = request.query as any;
+  getFullTextArticle(url).then((data) => {
+    if (!data.error && data.objects) {
+      server.io.emit('fulltextArticle', {
+        feedId,
+        id,
+        url,
+        data: data.objects[0],
+      });
+    }
+  });
 });
 
 server.get('/', async () => {
@@ -43,5 +56,6 @@ server.listen(process.env.PORT || 3000, '0.0.0.0', (err, address) => {
     console.error(err);
     process.exit(1);
   }
+  server.io.on('connect', (socket: any) => console.log('Socket connected!', socket.id));
   console.log(`Server listening at ${address}`);
 });
